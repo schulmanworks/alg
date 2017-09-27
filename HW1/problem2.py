5b2ffe74534bf3972249d1a67e01e2c36ccd0b17
Ryan Schulman <schulmanworks@gmail.com>
2017-09-26 18:51:07 -0600

    done with hw2. fixed bug

--- HW1/problem2.py
+++ HW1/problem2.py
@@ -12,6 +12,8 @@ def binarySearch(A, l, h, key):
     if l > h:
         return None
     m = int(math.floor((h+l)/2))
+    if m == len(A):
+        return None
     if A[m] == key:
         return A[m]
     elif A[m] < key:
@@ -23,21 +25,26 @@ if __name__ == "__main__":
     numShouldFind = 0
     numFound = 0
     times = []
-    r = range(50000, 100000000, 5000000)
+    r = range(1, 1000000, 5000)
     for x in r:
         arr = genArray(x)
         avg = 0
-        runs = 100
-        for v in range(runs):
+        runs = 1000
+        rng = range(runs)
+        for v in rng:
             num = np.random.randint(low=0, high=x)
             start_time = timeit.default_timer()
             found = binarySearch(arr, 0, x, num) is not None
             if found:
                 numFound += 1
+                avg += elapsed
+            else:
+                # we want a redo if we don't fine it
+                rng = range(runs + 1)
             if num in arr:
                 numShouldFind += 1
             elapsed = timeit.default_timer() - start_time
-            avg += elapsed
+
         avg = avg/runs
         print "avg elapsed time: %f" % (avg)
         times.append(avg)
