4ad201eaff63f7975240bb38c7a23d9f0015b8a6
Ryan Schulman <schulmanworks@gmail.com>
2017-11-08 15:37:11 -0700

    graphing working

--- HW3/problem1.py
+++ HW3/problem1.py
@@ -11,6 +11,10 @@ def getKthElement(k , arr):
     localMax = arr[localMaxIndex]
 
     # scan first portion
+    if len(arr) == 0:
+        return None
+    if len(arr) == 1:
+        return arr[0]
     for i in range(k):
         if arr[i] > localMax:
             localMax = arr[i]
@@ -26,9 +30,21 @@ def getKthElement(k , arr):
     return localMax
 
 if __name__ == "__main__":
-    arr = [random.randint(0,100) for _ in range(10)]
-    k = 3
-    print arr
-    print sorted(arr)
-    element = getKthElement(k, arr)
-    print element
+    avgs = []
+    v = 10000
+    k = 10
+    x_axs = range(k,v,1)
+    for x in x_axs:
+        avg = 0
+        r = 100
+        for __ in range(r):
+            arr = [random.randint(0,100) for _ in range(x)]
+            start_time = timeit.default_timer()
+            element = getKthElement(k, arr)
+            elapsed = timeit.default_timer() - start_time
+            avg += elapsed
+        avg /= float(r)
+        avgs.append(avg)
+
+    plt.plot(x_axs, avgs)
+    plt.show()
