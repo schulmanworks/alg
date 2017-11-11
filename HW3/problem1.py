4c9f5d8632f1ba3dca600a04fbafb26dda2847f4
Ryan Schulman <schulmanworks@gmail.com>
2017-11-11 15:53:45 -0700

    quicksort working

--- HW3/problem1.py
+++ HW3/problem1.py
@@ -29,18 +29,35 @@ def getKthElement(k , arr):
             del tmp_arr[0]
     return localMax
 
+def quickSort(arr, l, h):
+    if l < h:
+        part = partition(arr, l, h)
+        quickSort(arr, l, part - 1)
+        quickSort(arr, part+1, h)
+def partition(arr, l, h):
+    piv = arr[h]
+    i = l - 1
+    for j in range(l, h, 1):
+        if arr[j] <= piv:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[h] = arr[h], arr[i+1]
+    return i + 1
+
+
 if __name__ == "__main__":
     avgs = []
-    v = 10000
+    v = 1000
     k = 10
-    x_axs = range(k,v,1)
+    x_axs = range(k + 1,v,1)
     for x in x_axs:
         avg = 0
         r = 100
         for __ in range(r):
             arr = [random.randint(0,100) for _ in range(x)]
             start_time = timeit.default_timer()
-            element = getKthElement(k, arr)
+            quickSort(arr, 0, len(arr) - 1)
+            element = arr[k]#getKthElement(k, arr)
             elapsed = timeit.default_timer() - start_time
             avg += elapsed
         avg /= float(r)
