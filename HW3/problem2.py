40d1bf81bf518d2431a645f0013de8db102dd290
Ryan Schulman <schulmanworks@gmail.com>
2017-11-11 18:54:48 -0700

    done with problem2 timing

--- HW3/problem2.py
+++ HW3/problem2.py
@@ -49,11 +49,31 @@ def mergeSort(arr):
 
 if __name__ == "__main__":
     x = 10
-    # arr = [2, 4, 1, 3, 5]#
-    arr = [random.randint(0,100) for _ in range(x)]
+    arr = [2, 4, 1, 3, 5]#
+    # arr = [random.randint(0,100) for _ in range(x)]
     # print arr
     res = mergeSort(arr)
     print "$$$$$$$$$$$$$$"
-    print arr
-    print res
+    print "array",arr
+    print "result",res
     print "inversions:", inversions
+
+    avgs = []
+    v = 1000
+    k = 10
+    x_axs = range(k + 1,v,1)
+    for x in x_axs:
+        avg = 0
+        r = 100
+        for __ in range(r):
+            arr = [random.randint(0,100) for _ in range(x)]
+            start_time = timeit.default_timer()
+            res = mergeSort(arr)
+            elapsed = timeit.default_timer() - start_time
+            # print res
+            avg += elapsed
+        avg /= float(r)
+        avgs.append(avg)
+
+    plt.plot(x_axs, avgs)
+    plt.show()
